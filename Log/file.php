44f252476ec1a302d590a0d40c9c103d53ba35f3
Jon Parise <jon@php.net>
2003-02-06 05:21:54 +0000

    Silence errors from chmod() that may occur when the web server process doesn't actually own the log file.

--- Log/file.php
+++ Log/file.php
@@ -220,7 +220,7 @@ class Log_file extends Log
             return false;
         }
 
-        chmod($this->_filename, $this->_mode);
+        @chmod($this->_filename, $this->_mode);
 
         return true;
     }
