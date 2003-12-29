8de9b5fdd99f30866567cad003646d22892e5104
Jon Parise <jon@php.net>
2003-12-29 07:27:16 +0000

    Adding the latest package changes and switching to "simpleoutput" mode.

--- package.php
+++ package.php
@@ -3,16 +3,15 @@
 require_once 'PEAR/PackageFileManager.php';
 require_once 'Console/Getopt.php';
 
-$version = '1.8.1';
+$version = '1.8.2';
 $notes = <<<EOT
-Fixed some bugs in the 'win' handler.
-Updated the Log_observer::factory() method for consistency with Log::factory().
+Added a new 'null' log handler.
 EOT;
 
 $changelog = <<<EOT
-The 'win' handler now handles newline sequences correctly (Bug 282).
+A new 'null' log handler that simply consumes log events has been added.  The 'null' handler will still respect log masks and attached observers.
 
-The Log_observer::factory() method has been updated to accept an optional associative array of configuration values, return the newly-created object by reference, and look for files named 'Log/observer_$type.php'.  Backwards compatibility for the old-style conventions has been preserved.
+New unit tests for the Factory and Singleton construction methods have been added.
 EOT;
 
 $package = new PEAR_PackageFileManager();
@@ -24,10 +23,11 @@ $result = $package->setOptions(array(
     'state'             => 'stable',
     'license'           => 'PHP License',
     'filelistgenerator' => 'cvs',
-    'ignore'            => array('package.php', 'package.xml'),
+    'ignore'            => array('package.php'),
     'notes'             => $notes,
     'changelognotes'    => $changelog,
     'changelogoldtonew' => false,
+	'simpleoutput'		=> true,
     'baseinstalldir'    => '/',
     'packagedirectory'  => ''));
 
