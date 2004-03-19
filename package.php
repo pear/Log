b1322c75cdcb959ba5793b29101b663acd86ede2
Jon Parise <jon@php.net>
2004-03-19 08:30:09 +0000

    Updating the package.xml file to reflect the latest set of changes.

--- package.php
+++ package.php
@@ -3,23 +3,17 @@
 require_once 'PEAR/PackageFileManager.php';
 require_once 'Console/Getopt.php';
 
-$version = '1.8.4';
+$version = '1.8.5';
 $notes = <<<EOT
-The Log package now requires PHP 4.3.0 or later.
+The 'sql' handler now enforces a maximum $ident length of 16 characters.
 
-If an object or array is passed as a log event, its human-readable representation will be used.
-
-It's now possible to specify the default priority for events logged using the log() method.
+The 'sql' handler now maintains a separate sequence for each log table within the database.
 EOT;
 
 $changelog = <<<EOT
-The Log package now requires PHP 4.3.0 or later.
+The 'sql' handler now enforces a maximum $ident length of 16 characters.
 
-The _extractMessage() method no longer uses the serialize()'ed version of an event object if no string conversion method is available.  Instead, the human-readable (via print_r()) representation of the object will be used.
-
-_extractMessage() can now handle arrays.  Their human-readable representation will be used.
-
-Each Log instance now has a default priority level.  This priority will be used by the log() method when no specific priority parameter is provided.  It can be accessed using the new getPriority() and setPriority() methods.
+The 'sql' handler now maintains a separate sequence for each log table within the database.  You will need to take appropriate steps to preserve your sequence numbering if that is important to your site; the ID sequence will be reinitialized to 0 the first time this updated handler is used. (Submitted by Rob Lineweaver.)
 EOT;
 
 $package = new PEAR_PackageFileManager();
