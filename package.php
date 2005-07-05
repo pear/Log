be90de3bbf856c88f288638b3527c4728b696c28
Jon Parise <jon@php.net>
2005-07-05 01:42:05 +0000

    Adding a new MDB2 handler contributed by Lukas Smith.

--- package.php
+++ package.php
@@ -5,22 +5,24 @@ require_once 'PEAR/PackageFileManager.php';
 $version = '1.9.0';
 $notes = <<<EOT
 Added stringToPriority() for converting priority names to PEAR_LOG_* constants.
-The mail mail handler now uses \\r\\n instead of \\n to terminate lines.
+The mail handler now uses \\r\\n instead of \\n to terminate lines.
 The file handler will now perform file locking if the 'locking' parameter is set.
 The file hander's directory creation routines have been rewritten.
 The file handler will now set a created directory's permission mode based on the 'dirmode' parameter.
 The SQL handler can now be configured with an array of DB 'options'.
 The SQL handler now uses prepared queries for improved performance.
+An MDB2 database handler has been added.
 EOT;
 
 $changelog = <<<EOT
 Added stringToPriority() for converting priority names to PEAR_LOG_* constants. (Bug 2853)
-The mail mail handler now uses \\r\\n instead of \\n to terminate lines, per RFC 821. (Bug 4107)
+The mail handler now uses \\r\\n instead of \\n to terminate lines, per RFC 821. (Bug 4107)
 The file handler will now perform advsitory file locking (using flock()) if the 'locking' configuration parameter is set. (Bug 4064)
 The file hander's directory creation routines have been rewritten. (Bug 3989)
 The file handler will now set a created directory's permission mode based on the 'dirmode' parameter. (Bug 4114)
 The SQL handler can now be configured with an array of DB 'options'.
 The SQL handler now uses prepared queries for improved performance.
+An MDB2 database handler has been added. (Lukas Smith)
 EOT;
 
 $package = new PEAR_PackageFileManager();
