b0183d004d424c620dd4bda42c36d533e6923467
Jon Parise <jon@php.net>
2005-12-05 06:31:25 +0000

    Upgrade the packaging script to support package.xml 2.0.

--- package.php
+++ package.php
@@ -1,6 +1,11 @@
 <?php
 
-require_once 'PEAR/PackageFileManager.php';
+require_once 'PEAR/PackageFileManager2.php';
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
+
+$desc = <<<EOT
+The Log framework provides an abstracted logging system. It supports logging to console, file, syslog, SQL, Sqlite, mail, and mcal targets. It also provides a subject - observer mechanism.
+EOT;
 
 $version = '1.9.3';
 $notes = <<<EOT
@@ -8,40 +13,45 @@ The Observer code is now PHP 4.4 compliant. (Bug 5776)
 Log::factory(), Log::singleton(), and Log_observer::factory() now return null instead of false on failure.  This is more consistent with the model that these methods return object references.  Backwards compatibility is only violated if users were explicitly testing for a literal 'false' result.
 EOT;
 
-$changelog = $notes;
-
-$package = new PEAR_PackageFileManager();
+$package = new PEAR_PackageFileManager2();
 
 $result = $package->setOptions(array(
-    'package'           => 'Log',
-    'summary'           => 'Logging utilities',
-    'version'           => $version,
-    'state'             => 'stable',
-    'license'           => 'PHP License',
     'filelistgenerator' => 'cvs',
-    'ignore'            => array('package.php', 'phpdoc.sh'),
-    'notes'             => $notes,
-    'changelognotes'    => $changelog,
     'changelogoldtonew' => false,
 	'simpleoutput'		=> true,
     'baseinstalldir'    => '/',
-    'packagedirectory'  => ''));
+    'packagedirectory'  => '.'));
 
 if (PEAR::isError($result)) {
     echo $result->getMessage();
     die();
 }
 
-$package->addMaintainer('jon', 'lead', 'Jon Parise', 'jon@php.net');
+$package->setPackage('Log');
+$package->setPackageType('php');
+$package->setSummary('Logging utilities');
+$package->setDescription($desc);
+$package->setChannel('pear.php.net');
+$package->setLicense('PHP License', 'http://www.php.net/license/3_01.txt');
+$package->setAPIVersion('1.0.0');
+$package->setAPIStability('stable');
+$package->setReleaseVersion($version);
+$package->setReleaseStability('stable');
+$package->setNotes($notes);
+$package->setPhpDep('4.3.0');
+$package->setPearinstallerDep('1.4.3');
+$package->addMaintainer('lead',  'jon', 'Jon Parise', 'jon@php.net');
+$package->addIgnore(array('package.php', 'phpdoc.sh'));
 
-$package->addDependency('php', '4.3.0', 'ge', 'php');
-$package->addDependency('DB', '1.3', 'ge', 'pkg', true);
-$package->addDependency('sqlite', false, 'has', 'ext', true);
+$package->generateContents();
+$package1 = &$package->exportCompatiblePackageFile1();
 
 if ($_SERVER['argv'][1] == 'commit') {
     $result = $package->writePackageFile();
+    $result = $package1->writePackageFile();
 } else {
     $result = $package->debugPackageFile();
+    $result = $package1->debugPackageFile();
 }
 
 if (PEAR::isError($result)) {
