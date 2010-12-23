26b89921ad2167ed1cc40d2c00d6662dccd64c51
Jon Parise <jon@php.net>
2010-12-23 23:24:15 +0000

    Improving the console handler's stream handling.
    The system-level STDOUT descriptor isn't always available.  If it's not,
    create our own output stream (via php://output) instead.
    
    Bug: 17874
    Patch by: Eric Caron

--- package.php
+++ package.php
@@ -7,10 +7,9 @@ $desc = <<<EOT
 The Log package provides an abstracted logging framework.  It includes output handlers for log files, databases, syslog, email, Firebug, and the console.  It also provides composite and subject-observer logging mechanisms.
 EOT;
 
-$version = '1.12.5';
+$version = '1.12.6';
 $notes = <<<EOT
-- Corrected the "levels" unit test on 32-bit systems. (Bug 18099)
-- Corrected quote handling in Firebug console output. (Bug 18141)
+- Improved the console handler's stream handling. (Bug 17874)
 EOT;
 
 $package = new PEAR_PackageFileManager2();
